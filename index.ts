98bcb4453773dc95f0e0635c2565873385fe78dc
umarghafoor420 <164578000+umarghafoor420@users.noreply.github.com>
2024-08-05 03:29:49 +0500

    Add files via upload
    This is a todo list  app which make  list  of any thing that you want

---
+++ index.ts
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let ToDos = []
+
+let condition=true;
+while(condition){
+ let addtask= await inquirer.prompt([
+{
+          name:"ToDos",
+          type:"input",
+          message:"What you want to add in your todos?"
+},
+          {
+            name:"AddMore",
+            type:"confirm",
+            message:"Do you want to add more?",
+            default:"true"
+          }
+]);
+ToDos.push(addtask.ToDos);
+condition=addtask.AddMore
+console.log(ToDos);
+}
